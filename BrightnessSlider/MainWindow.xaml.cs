aadbf4518d89bc14fd55d255b058daa165eff556
Slava Logos <xivilaim@gmail.com>
2019-10-26 12:16:40 +0300

    adding slider and ability to set brightness

--- BrightnessSlider/MainWindow.xaml.cs
+++ BrightnessSlider/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,13 +16,64 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BrightnessSlider {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow()
         {
             InitializeComponent();
+
+            // set initial position
+            Left = SystemParameters.WorkArea.Width - (Width + 5);
+            Top = SystemParameters.WorkArea.Height - Height;
+
+            ShowInTaskbar = false;
+            Topmost = true;
+
+            //notifyIcon1.MouseClick += NotifyIcon1_MouseClick;
+            //Deactivate += Form1_Deactivate;
+
+            //CreateNotifyIConContexMenu();
+
+            //Brightness = Brightness; // set initial brightness
+            slider.ValueChanged += Slider_ValueChanged;
         }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Brightness = Convert.ToInt32(slider.Value);
+        }
+
+        public int Brightness {
+            get {
+                ManagementScope scope = new ManagementScope("root\\WMI");
+                SelectQuery query = new SelectQuery("WmiMonitorBrightness");
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query)) {
+                    using (ManagementObjectCollection objectCollection = searcher.Get()) {
+                        foreach (ManagementObject mObj in objectCollection) {
+                            var br_obj = mObj.Properties["CurrentBrightness"].Value;
+                            int br = 0;
+                            int.TryParse(br_obj + "", out br);
+                            return br;
+                        }
+                    }
+                }
+                return 0;
+            }
+            set {
+                ManagementScope scope = new ManagementScope("root\\WMI");
+                SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query)) {
+                    using (ManagementObjectCollection objectCollection = searcher.Get()) {
+                        foreach (ManagementObject mObj in objectCollection) {
+                            mObj.InvokeMethod("WmiSetBrightness",
+                                new Object[] { UInt32.MaxValue, value });
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
